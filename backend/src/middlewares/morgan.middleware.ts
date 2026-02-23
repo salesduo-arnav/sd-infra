f86a23e2e604c00d433c447c1d06086e42dae5c5
salesduo-arnav <arnav@salesduo.com>
2026-02-23 13:34:31 +0530

    fixed middleware reviews

--- backend/src/middlewares/morgan.middleware.ts
+++ backend/src/middlewares/morgan.middleware.ts
@@ -1,11 +1,15 @@
 import morgan, { StreamOptions } from "morgan";
+import { Request, Response } from "express";
 import Logger from "../utils/logger";
 
 const stream: StreamOptions = {
     write: (message) => Logger.http(message),
 };
 
-const skip = () => {
+const skip = (req: Request, res: Response) => {
+    if (req.originalUrl === '/health' || req.originalUrl === '/api/health') {
+        return true;
+    }
     return false;
 };
 
